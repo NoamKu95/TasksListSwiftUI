8499db69292fdf0edd0975dea4afbe78f4f3e60f
Noam Kurtzer <noamkurtzer@gmail.com>
2023-03-19 19:26:18 +0200

    Created BlankView to darken the screen when NewTaskForm is showing

--- TasksList/TasksList/Screens/ContentView.swift
+++ TasksList/TasksList/Screens/ContentView.swift
@@ -81,6 +81,7 @@ struct ContentView: View {
                 
                 // MARK: - NEW TASK FORM
                 if shouldShowAddTask {
+                    BlankDarkView()
                     NewTaskFormView()
                 }
                 
